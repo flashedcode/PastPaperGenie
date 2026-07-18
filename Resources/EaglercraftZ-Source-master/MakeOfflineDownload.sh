19e14b01527dbb697cf856f8ec2d2fa24947a470
Flashedcode <148499988+flashedcode@users.noreply.github.com>
2026-07-18 08:31:43 +0100

    Delete Resources/EaglercraftZ-Source-master directory

--- Resources/EaglercraftZ-Source-master/MakeOfflineDownload.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-java -cp "desktopRuntime/MakeOfflineDownload.jar:desktopRuntime/CompileEPK.jar" net.lax1dude.eaglercraft.v1_8.buildtools.workspace.MakeOfflineDownload "javascript/OfflineDownloadTemplate.txt" "javascript/classes.js" "javascript/assets.epk" "javascript/EaglercraftZ_1.20_Offline_en_US.html" "javascript/EaglercraftZ_1.20_Offline_International.html" "javascript/lang"
